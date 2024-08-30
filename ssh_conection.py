82fd544bb1e49fe1e310bc01400323c46f348991
Javier1600 <javiandrade83@gmail.com>
2024-08-30 16:01:21 -0500

    Final

--- ssh_conection.py
+++ ssh_conection.py
@@ -130,9 +130,9 @@ with open(nAccess, 'w', encoding="utf-8") as errors_file:
             ssh_client.close()
 
 # Validar y crear directorios en el servidor FTP
-ftp_server = '190.110.195.146'  # Cambia esto por la dirección de tu servidor FTP
-ftp_username = 'forti_mng'       # Cambia esto por tu nombre de usuario FTP
-ftp_password = 'FW_BK$$2024'     # Cambia esto por tu contraseña FTP
+ftp_server = '190.110.195.146'
+ftp_username = 'forti_mng'
+ftp_password = 'FW_BK$$2024'
 ensure_ftp_directories(ftp_server, ftp_username, ftp_password, ['FTP', 'BackUps',f'BK_SIMED_{date}'])
 
 # Llamar a la función para subir los archivos
